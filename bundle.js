02b6244c29cd359763e1c11645f27196ea043983
NEWHELL69 <kushagra0304@gmail.com>
2023-05-07 10:31:21 +0530

    pilot

---
+++ bundle.js
@@ -0,0 +1,43 @@
+(function(){
+    const menu = document.querySelector(".menu");
+    const menuButton = document.querySelector("#menu__toggle");
+    menuButton.addEventListener("click", () => {
+        if(menu.getAttribute("data-open") == null) {
+            menu.setAttribute("data-open", "");
+        } else {
+            menu.removeAttribute("data-open");
+        }
+    })
+})();         
+
+(function(){
+    const dropdowns = document.querySelectorAll(".menu details");
+
+    dropdowns.forEach((dropdown) => {
+        dropdown.addEventListener("mouseenter", () => {
+            dropdown.setAttribute("open", "");
+        });
+
+        dropdown.addEventListener("mouseleave", () => {
+            dropdown.removeAttribute("open");
+        });
+    })
+})();           
+(function () {
+    let frameNumber = 1;
+    
+    const carouselToggle = document.querySelector("#carousel-toggle");
+    const carouselsInner = document.querySelectorAll(".carousel .carousel-inner");
+
+    carouselToggle.addEventListener("click", (e) => {
+        carouselsInner.forEach((carousel) => {
+            carousel.classList.remove(`carousel-inner-${frameNumber}`)
+        })
+
+        frameNumber = (frameNumber%carouselsInner.length) + 1;
+
+        carouselsInner.forEach((carousel) => {
+            carousel.classList.add(`carousel-inner-${frameNumber}`)
+        })
+    });
+})() 
